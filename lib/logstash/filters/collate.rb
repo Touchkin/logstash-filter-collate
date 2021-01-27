955876a0ce68fe74812120e659ac7ab6799d45f3
ShubhankarS <shubhankar@touchkin.com>
2021-01-27 09:01:04 +0000

    another log

--- lib/logstash/filters/collate.rb
+++ lib/logstash/filters/collate.rb
@@ -61,6 +61,7 @@ class LogStash::Filters::Collate < LogStash::Filters::Base
     # if the event is collated, a "collated" tag will be marked, so for those uncollated event, cancel them first.
     if event.get("tags").nil? || !event.get("tags").include?("collated")
       event.cancel
+      @logger.info("cencelling event", :event => event)
     else
       return
     end
